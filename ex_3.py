c62ee4dda8381b5ff07df9f860eb9d56f3248b77
Rarche <ramil13@inbox.ru>
2021-11-26 14:13:24 +0700

    added ex_3

---
+++ ex_3.py
@@ -0,0 +1,12 @@
+user_number = input("Введите целое положительное число: ")
+
+var_2 = user_number * 2
+var_3 = user_number * 3
+
+user_number = int(user_number)
+var_2 = int(var_2)
+var_3 = int(var_3)
+
+summ_of_number = (user_number + var_2 + var_3)
+
+print(f"Сумма чисел:{summ_of_number}")
