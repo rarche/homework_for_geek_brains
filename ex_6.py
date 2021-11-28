ed5cf7b530e7746ec6a734ff73f763caac58cce0
Rarche <ramil13@inbox.ru>
2021-11-28 21:18:14 +0700

    correct ex_6

--- ex_6.py
+++ ex_6.py
@@ -1,9 +1,9 @@
-a = 3
-b = 9
-c = 1
+distance_1 = float(input('Введите дистанцию на первый день: '))
+distance_2 = float(input('Целевая дистанция: '))
+amount_of_days = 1
 
-while a < b:
-    a = a * 1.1
-    c += 1
+while distance_1 < distance_2:
+    distance_1 = distance_1 * 1.1
+    amount_of_days += 1
 
-print(c)
+print(f'Требуемое количество дней: {amount_of_days}')
