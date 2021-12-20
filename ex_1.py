052b96eb38da36d1648d8435a7335f3a9b4e81ad
Rarche <ramil13@inbox.ru>
2021-12-20 15:09:25 +0700

    Commit all ex

--- ex_1.py
+++ ex_1.py
@@ -1,6 +1,10 @@
 from sys import argv
-hours = int(argv[1])
-money_per_hour = int(argv[2])
-bonus = int(argv[3])
-result = hours * money_per_hour + bonus
-print(result)
+
+if len(argv) < 4:
+    print('Необходимо ввести все данные')
+else:
+    hours = int(argv[1])
+    money_per_hour = int(argv[2])
+    bonus = int(argv[3])
+    result = hours * money_per_hour + bonus
+    print(result)
